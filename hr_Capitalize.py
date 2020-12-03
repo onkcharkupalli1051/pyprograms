c31493d78553abb5651bb6ed69fd8e73fa54e1c3
Onkar <onkarcharkupalli@gmail.com>
2020-12-03 22:32:47 +0530

    BANANA

---
+++ hr_Capitalize.py
@@ -0,0 +1,57 @@
+"""
+You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
+
+
+Given a full name, your task is to capitalize the name appropriately.
+
+Input Format
+
+A single line of input containing the full name, .
+
+Constraints
+
+The string consists of alphanumeric characters and spaces.
+Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+
+Output Format
+
+Print the capitalized string, .
+
+Sample Input
+
+chris alan
+Sample Output
+
+Chris Alan
+"""
+import string
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+
+def solve(s):
+    l = list(s.split(" "))
+    nl = []
+    for ele in l:
+        nl.append(ele[:1].capitalize() + ele[1:])
+    s = " ".join(nl)
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
---
+++ pe_multiples_of_3_and_5.py
@@ -0,0 +1,15 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+end = int(input())
+l = []
+sum = 0
+for i in range(1,end):
+    if(i%3 == 0 or i%5 ==0 ):
+        sum += i
+        l.append(i)
+
+print("sum : ",sum)
+print("list : ",l)
